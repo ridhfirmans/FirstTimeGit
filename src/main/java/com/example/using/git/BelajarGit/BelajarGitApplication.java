807f559cb0b98ea6c639c65c5dc199a4eb6f6446
Ridho Firmansyah <ridho.firmansyah33@gmail.com>
2018-11-19 13:45:53 +0700

    Commit pertama

---
+++ src/main/java/com/example/using/git/BelajarGit/BelajarGitApplication.java
@@ -0,0 +1,12 @@
+package com.example.using.git.BelajarGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarGitApplication.class, args);
+	}
+}
